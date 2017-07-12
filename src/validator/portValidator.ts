ef827e76cc1ebfa693bc18a1bde7767672b5f57a
Alex Alonso <alex4551@gmail.com>
2017-07-12 11:13:15 +0800

    fix parseInt radix

--- src/validator/portValidator.ts
+++ src/validator/portValidator.ts
@@ -1,7 +1,7 @@
 export class PortValidator {
     public static validate(port: any) {
         let result = false;
-        port = parseInt(port, 0);
+        port = parseInt(port, 10);
         if (port > 0 && port < 65535) {
             result = port;
         }
