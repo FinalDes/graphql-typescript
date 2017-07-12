0997a071dfb9e4d52b6b36a16b4708edcbf2a150
Alex Alonso <alex4551@gmail.com>
2017-07-12 12:09:01 +0800

    update test configuration

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,14 +3,36 @@ import request = require("supertest");
 import {App} from "./index";
 
 describe("Graphql test", () => {
-  it("Get Hi", (done) => {
+  it("Get Hi, content-type application/json", (done) => {
     request(App)
     .post("/graphql")
     .set("Content-Type", "application/json; charset=UTF-8")
     .send({query: "{hi}"})
     .expect(200)
     .end((err, res) => {
-      res.text.should.to.equal(JSON.stringify({data: {hi: "Hello Word"}}));
+      res.body.should.to.deep.equal({data: {hi: "Hello Word"}});
+      done();
+    });
+  });
+  it("Get Hi, content-type application/x-www-form-urlencoded", (done) => {
+    request(App)
+    .post("/graphql")
+    .set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+    .send({query: "{hi}"})
+    .expect(200)
+    .end((err, res) => {
+      res.body.should.to.deep.equal({data: {hi: "Hello Word"}});
+      done();
+    });
+  });
+  it("Get Hi, content type application/graphql", (done) => {
+    request(App)
+    .post("/graphql")
+    .set("Content-Type", "application/graphql; charset=UTF-8")
+    .send("{hi}")
+    .expect(200)
+    .end((err, res) => {
+      res.body.should.to.deep.equal({data: {hi: "Hello Word"}});
       done();
     });
   });
