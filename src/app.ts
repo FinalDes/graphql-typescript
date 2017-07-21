3abefbd811d228a85945814c5c2aa903dd3efe2a
Alex Alonso <alex4551@gmail.com>
2017-07-21 11:41:28 +0800

    update app layout

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,11 @@
-/* tslint:disable:no-console*/
 import express = require("express");
 import helmet = require("helmet");
 import graphqlHTTP = require("express-graphql");
 
 import {schema} from "./schema";
-import dotenv = require("dotenv");
-import { PortValidator } from "./validator/portValidator";
-dotenv.config();
 
-const port: any = PortValidator.validate(process.env.PORT, 3000);
 const App = express();
-// app.use(helmet.noCache());
-// app.use(helmet.dnsPrefetchControl());
-// app.use(helmet.frameguard({ action: "deny" }));
-// app.use(helmet.referrerPolicy({ policy: "no-referrer" }));
+App.use(helmet());
 
 App.use("/graphql", graphqlHTTP({
   graphiql: true,
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
-import app from "app";
+/* tslint:disable:no-console*/
+import app from "./app";
 
+import dotenv = require("dotenv");
+import { PortValidator } from "./validator/portValidator";
+dotenv.config();
+
+const port: any = PortValidator.validate(process.env.PORT, 3000);
 app.listen(port, () => {
   console.log(`Listening port ${port}`);
 });
