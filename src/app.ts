efd6bd2dbf0a0044f2c817939b5badfd9575ce7d
Alex Alonso <alex4551@gmail.com>
2017-07-21 11:38:17 +0800

    use nodemon change test runner to jest

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-console*/
 import express = require("express");
-// import helmet = require("helmet");
+import helmet = require("helmet");
 import graphqlHTTP = require("express-graphql");
 
 import {schema} from "./schema";
@@ -9,19 +9,15 @@ import { PortValidator } from "./validator/portValidator";
 dotenv.config();
 
 const port: any = PortValidator.validate(process.env.PORT, 3000);
-const app = express();
+const App = express();
 // app.use(helmet.noCache());
 // app.use(helmet.dnsPrefetchControl());
 // app.use(helmet.frameguard({ action: "deny" }));
 // app.use(helmet.referrerPolicy({ policy: "no-referrer" }));
 
-app.use("/graphql", graphqlHTTP({
+App.use("/graphql", graphqlHTTP({
   graphiql: true,
   schema,
 }));
 
-app.listen(port, () => {
-  console.log(`Listening port ${port}`);
-});
-
-export const App: any = app;
+export default App;
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,40 +1,40 @@
 /* tslint:disable:no-console*/
 import request = require("supertest");
-import {App} from "./index";
+import App from "./app";
 
 const result = {data: {hi: "Hello Word"}};
 
 describe("Graphql test", () => {
-  it("Get Hi, content-type application/json", (done) => {
+  test("Get Hi, content-type application/json", (done) => {
     request(App)
     .post("/graphql")
     .set("Content-Type", "application/json; charset=UTF-8")
     .send({query: "{hi}"})
     .expect(200)
     .end((err, res) => {
-      res.body.should.to.deep.equal(result);
+      expect(res.body).toEqual(result);
       done();
     });
   });
-  it("Get Hi, content-type application/x-www-form-urlencoded", (done) => {
+  test("Get Hi, content-type application/x-www-form-urlencoded", (done) => {
     request(App)
     .post("/graphql")
     .set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
     .send({query: "{hi}"})
     .expect(200)
     .end((err, res) => {
-      res.body.should.to.deep.equal(result);
+      expect(res.body).toEqual(result);
       done();
     });
   });
-  it("Get Hi, content type application/graphql", (done) => {
+  test("Get Hi, content type application/graphql", (done) => {
     request(App)
     .post("/graphql")
     .set("Content-Type", "application/graphql; charset=UTF-8")
     .send("{hi}")
     .expect(200)
     .end((err, res) => {
-      res.body.should.to.deep.equal(result);
+      expect(res.body).toEqual(result);
       done();
     });
   });
