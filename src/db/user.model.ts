14c437176271a3d728e9a8f9331563c8cb25bc34
Alex Alonso <alex4551@gmail.com>
2017-07-21 17:05:25 +0800

    add user query

--- src/db/user.model.ts
+++ src/db/user.model.ts
@@ -7,13 +7,13 @@ export const Users = mongoose.model("Users", new Schema({
     password: String,
 }));
 
-const user1 = new Users({email: "alex", password: "123"});
+// const user1 = new Users({email: "alex", password: "123"});
 
-user1.save((err) => {
-    if (err) {
-        console.log(err);
-    }
-});
+// user1.save((err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
 
 // console.log(Users.findById("5971abd60eac6c7d1aedc8b3").exec((doc, err) => {
 //     console.log(err, "err");
--- src/schema.ts
+++ src/schema.ts
@@ -10,45 +10,58 @@ import {
 } from "graphql";
 import {Users} from "./db/user.model";
 
-const User = new GraphQLObjectType({
+const UserSchema = new GraphQLObjectType({
+    name: "User",
     fields: {
-        id: {
-            type: GraphQLID,
-        },
-        email: {
-            type: GraphQLString,
-        },
-        password: {
-            type: GraphQLString,
-        },
+        id: {type: GraphQLID},
+        email: {type: GraphQLString},
+        password: {type: GraphQLString},
     },
-     name: "User",
 });
 
 const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
     fields: {
         getUsers: {
              args: {
                  limit: {type: GraphQLInt},
                  email: {type: GraphQLString},
                 },
-            type: new GraphQLList(User),
+            type: new GraphQLList(UserSchema),
             resolve(parentValue: any, args: any) {
                 const limit: number = args.limit && args.limit < 15 ? args.limit : 5;
-                return Users.find({email: args.email}).limit(limit).exec();
+                const query = args.email ? {email: args.email} : {};
+                return Users.find({}).limit(limit).exec();
             },
         },
         getUser: {
             args: {id: {type: new GraphQLNonNull(GraphQLString)}},
-            type: User,
+            type: UserSchema,
             resolve(parentValue: any, args: any) {
                 return Users.findById(args.id).exec();
             },
         },
     },
-    name: "RootQueryType",
+});
+
+const mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        createUser: {
+            args: {
+                email: {type: new GraphQLNonNull(GraphQLString)},
+                password: {type: new GraphQLNonNull(GraphQLString)},
+            },
+            type: UserSchema,
+            resolve(parentValue: any, args: any) {
+                const newUser = new Users(args);
+                return newUser.save();
+            },
+        },
+    },
 });
 
 export const schema = new GraphQLSchema({
     query: RootQuery,
+    mutation,
 });
